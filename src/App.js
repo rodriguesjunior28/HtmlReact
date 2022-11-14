98b412ca322c9c442d9c718c9ac428b4ea76fa57
Squadrão 17 <111467580+Squad017@users.noreply.github.com>
2022-11-13 23:48:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Destino from './pages/Destino';
+import Promocao from './pages/promocao/Promocao';
+import Contato from './pages/Contato';
+import Header from './layout/Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import QuemSomos from './pages/QuemSomos';
+import NossaMissao from './pages/NossaMissao';
+import Visao from './pages/Visao';
+import Cadastro from './pages/Cadastro';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Routes>
+          <Route path='/' element={<Home /> } />
+          <Route path='/Destino' element={<Destino />} />
+          <Route path='/Promocao' element={<Promocao />} />
+          <Route path='/Contato' element={<Contato />} />
+          <Route path='/Quemsomos' element={<QuemSomos />} />
+          <Route path='/NossaMissao' element={<NossaMissao />} />
+          <Route path='/Visao' element={<Visao />} />
+          <Route path='/Cadastro' element={<Cadastro />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
